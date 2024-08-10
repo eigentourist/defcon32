4968f9895da7676ac9db058878c3f8f880e7865e
acz <zeitnot@yahoo.com>
2024-08-09 22:14:17 -0700

    renaming for clarity

--- perceptrons/simple/build-perceptron-gate.sh
+++ perceptrons/simple/build-perceptron-gate.sh
@@ -2,11 +2,11 @@
 
 # Invoke GNU C compiler to produce a source listing with assembly code
 # and an object file, but not a linked executable file.
-gcc -S -fverbose-asm -g -o perceptron_01.s perceptron_01.c
+gcc -S -fverbose-asm -g -o perceptron_02.s perceptron_02.c
 
 # Invoke c++filt to read the source listing and produce a more readable
 # assembler listing with C statements paired to corresponding instructions.
-c++filt < perceptron_01.s | as -alhnd > perceptron_01.lst
+c++filt < perceptron_02.s | as -alhnd > perceptron_02.lst
 
 # Invoke GNU C compiler to build an executable program with debug symbols.
-gcc -g -o perceptron_01 perceptron_01.c -lm
+gcc -g -o perceptron_gate perceptron_gate.c -lm
