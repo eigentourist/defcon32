0612a485edd551214c449967c0d26a25b9e6492b
acz <zeitnot@yahoo.com>
2024-08-09 21:44:19 -0700

    Cleanup

--- perceptrons/multilayer/build-multilayer-flare.sh
+++ perceptrons/multilayer/build-multilayer-flare.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Invoke GNU C compiler to build an executable program with debug symbols.
-g++ -std=c++11 multilayer_01.cpp -o multilayer_01
+g++ -std=c++11 multilayer_flare.cpp -o multilayer_flare
--- perceptrons/multilayer/build-multilayer-xor.sh
+++ perceptrons/multilayer/build-multilayer-xor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Invoke GNU C compiler to build an executable program with debug symbols.
-g++ -std=c++11 multilayer_00.cpp -o multilayer_00
+g++ -std=c++11 multilayer_xor.cpp -o multilayer_xor
